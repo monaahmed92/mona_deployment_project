55837b4867d8c21f047f7fa230f074d74744ff5a
mona.ahmed <mona.elkourany92@gmail.com>
2025-12-22 11:59:27 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,11 @@
+
 terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 4.65"  # use latest v4 compatible with your modules
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
